c71928151059c282481ac6598af82ac29aae1d61
haithemamir <122388788+haithemamir@users.noreply.github.com>
2024-02-14 00:58:11 -0500

    Create mips.s
    Added psuedo code outline for project

---
+++ mips.s
@@ -0,0 +1,12 @@
+## X = 03006740
+# N = 26 + (X % 11) = 26
+# M = N - 10 = 16
+# 0-9, a-p and A-P
+
+# phase 1: write the subroutine to process a single string
+# pass in a hard coded string IE 123hello and make sure that subroutine
+# calculates the correct value
+
+# phase2: read a string from the user and break it up based on the /
+
+# phase3: pass each broken up string into the subroutine from phase 1
